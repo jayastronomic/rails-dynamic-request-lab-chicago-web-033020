fb566105c92ad63ef41c6975f8e60d65a942a596
jayastronomic <juliansmith94@hotmail.com>
2020-04-28 01:09:33 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Julian", last_name: "Smith")
+Student.create(first_name: "Bunice", last_name:"Jones")
